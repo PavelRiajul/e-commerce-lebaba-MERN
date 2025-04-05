736b7bdf22d45003633b733463ce6a3d0d577572
Riajul Islam <riajulpislam@gmail.com>
2025-04-05 13:31:47 +0600

    product model complete

--- backend/src/products/product.controller.js
+++ backend/src/products/product.controller.js
@@ -60,7 +60,59 @@ const getAllProducts =async(req,res)=>{
     return errorResponse(res, 500, "Failed to get all products", error)
  }
 }
+//get single products
+const getSingleProduct=async(req,res)=>{
+    const {id} = req.params;
+   try {
+    const product  = await Products.findById(id).populate('author', 'username email');
+    
+    if(!product) {
+        return errorResponse(res, 404, "Product not found")
+    }
+    const reviews=  await Reviews.find({productId: id}).populate('userId', 'username email')
+
+    return successResponse(res, 200, "Single Product and reviews ",{product, reviews})
+
+   } catch (error) {
+    return errorResponse(res, 500, "Failed to get single product", error)
+   }
+}
+//update product
+const updateProductById =async(req,res)=>{
+    const productId =  req.params.id;
+    try {
+        const updatedProduct =  await Products.findByIdAndUpdate(productId, {...req.body}, {
+            new: true
+        })
+        if(!updatedProduct) {
+            return errorResponse(res, 404, "Product not found")
+        }
+        return successResponse(res, 200, "Product updated successfully", updatedProduct)
+    } catch (error) {
+        return errorResponse(res, 500, "Failed to update", error)
+    }
+}
+
+//delete product
+const deleteProductById=async(req,res)=>{
+    const productId = req.params.id;
+    try {
+        const deletedProduct = await Products.findByIdAndDelete(productId);
+
+        if(!deletedProduct) {
+            return errorResponse(res, 404, "Product not found")
+        }
+        await Reviews.deleteMany({productId: productId});
+        return successResponse(res, 200, "Product deleted successfully")
+    } catch (error) {
+        return errorResponse(res, 500, "Failed to delete", error)
+    }
+}
 module.exports={
     createNewProduct,
     getAllProducts,
+    getSingleProduct,
+    updateProductById,
+    deleteProductById
+
 }
