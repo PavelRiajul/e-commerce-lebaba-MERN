7a2e2a3d3af1231b2a42db5b17b27c88b959c697
Riajul Islam <riajulpislam@gmail.com>
2025-04-13 21:54:42 +0600

    confirm order

--- backend/src/orders/order.controller.js
+++ backend/src/orders/order.controller.js
@@ -1,6 +1,6 @@
 const { BASE_URL } = require("../utils/baseURL");
-const { errorResponse } = require("../utils/responseHandler");
-
+const { errorResponse, successResponse } = require("../utils/responseHandler");
+const Order = require("./order.model");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const makePaymentRequest = async (req, res) => {
@@ -30,6 +30,47 @@ const makePaymentRequest = async (req, res) => {
     return errorResponse(res, 500, "Failed to create payment session", error);
   }
 };
+
+const confirmPayment = async (req, res) => {
+  const {session_id} = req.body;
+  console.log(session_id)
+  try {
+    const session =  await stripe.checkout.sessions.retrieve(session_id, {
+      expand: ["line_items", "payment_intent"]
+    })
+    const paymentIntentId = session.payment_intent.id;
+    let order =  await Order.findOne({orderId: paymentIntentId})
+
+    if(!order){
+      const lineItems = session.line_items.data.map((item) => ({
+        productId: item.price.product,
+        quantity: item.quantity
+      }))
+
+      const amount = session.amount_total / 100;
+      
+      order= new Order({
+        orderId: paymentIntentId,
+        products:lineItems,
+        amount: amount,
+        email: session.customer_details.email,
+        status: session.payment_intent.status === "succeeded" ? "pending" : "failed",
+      })
+
+    } else {
+      order.status = session.payment_intent.status === "succeeded" ? "pending" : "failed"
+    }
+
+    await order.save()
+    return successResponse(res, 200, "Order confirmed successfully", order)
+
+
+  } catch (error) {
+    return errorResponse(res, 500, "Failed to confirmed payment", error);
+  }
+}
+
 module.exports = {
   makePaymentRequest,
+  confirmPayment,
 };
